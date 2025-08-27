19aa8648b08ae7256c3164d7071dd39a47b75a5f
Jonathan Souza Moyano <107486466+JonathanMoyano@users.noreply.github.com>
2025-08-26 21:56:54 -0300

    Update cv-data.js

--- src/data/cv-data.js
+++ src/data/cv-data.js
@@ -41,7 +41,7 @@ export const cvData = {
       title: "Windows Prompt: Utilizando o CMD",
       institution: "Alura",
       date: "Ago de 2025",
-      category: "Linux",
+      category: "Windows",
       link: "https://cursos.alura.com.br/certificate/858009a3-028d-49f0-9ecf-677495f5a4ea",
       credentialId: "858009a3-028d-49f0-9ecf-677495f5a4ea",
       description: "Curso focado no aprendizado de comandos do prompt do Windows (CMD) para executar tarefas e otimizar o fluxo de trabalho no dia a dia."
